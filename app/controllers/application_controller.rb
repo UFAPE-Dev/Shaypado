512f46616febd56373aad49d5fb2238c58f41233
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2022-09-22 15:00:22 -0300

    Implementado validações do model de usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :email, :endereco, :cep, :cpf, :data_nascimento, :contato, :horario_trabalho])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :email, :endereco, :cep, :cpf, :data_nascimento, :contato, :horario_trabalho])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   enum role: [:proprietario, :instrutor, :aluno]
   after_initialize :set_default_role, :if => :new_record?
-  validates :nome, presence: true
+  validates :nome, presence: true, length: { maximum: 100, minimum: 10 }, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/, message: "Somente letras"}
+  validates :cpf, presence: true, length: { is: 11 }, uniqueness: true, format: { with: /\A[0-9]+\z/, message: "Somente números" }
+  validates :data_nascimento, presence: true, comparison: {less_than: Date.today, greater_than: Date.new(1850, 1, 1)}
+  validates :cep, presence: true, length: { maximum: 8 }, format: { with: /\A\d{8}\z/ }
+  validates :endereco, presence: true, length: { maximum: 100, minimum: 5 }
+  validates :contato, presence: true, length: { is: 11 }, format: { with: /\A[0-9]+\z/, message: "Somente números" }
+  validates :horario_trabalho, presence: true
+
   def set_default_role
     self.role ||= :proprietario
   end
