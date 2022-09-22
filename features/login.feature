a67d175126eb2aaf521b91f0941dd97bda076423
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2022-09-22 16:56:02 -0300

    Segundo cenário da feature Login

--- features/login.feature
+++ features/login.feature
@@ -9,3 +9,13 @@ Feature: login
     And eu clico no botao Sign Up
     Then eu vejo a mensagem 'Welcome! You have signed up successfully.'
 
+  Scenario: login com sucesso
+    Given eu estou na pagina de cadastro
+    When preencho meu email 'email@email.com' e senha '123456senha'
+    And eu clico no botao Sign Up
+    And eu faco logout
+    And eu estou na pagina de login
+    When eu preencho o campo email com 'email@email.com' e senha '123456senha'
+    And eu clico no botao Log in
+    Then eu vejo a mensagem 'Signed in successfully.'
+
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,5 +1,3 @@
-
-
 Given('eu estou na pagina de cadastro') do
   visit '/users/sign_up'
   expect(page).to have_content 'Sign up'
@@ -26,4 +24,23 @@ end
 
 Then('eu vejo a mensagem {string}') do |message|
   expect(page).to have_content message
+end
+
+Given('eu estou na pagina de login') do
+  visit '/users/sign_in'
+  expect(page).to have_content 'Log in'
+end
+
+When('eu preencho o campo email com {string} e senha {string}') do |email, senha|
+  fill_in 'user[email]', :with => email
+  fill_in 'user[password]', :with => senha
+end
+
+And('eu clico no botao Log in') do
+  click_button 'Log in'
+end
+
+And('eu faco logout') do
+  click_on 'Logout'
+  expect(page).to have_content 'Bem vindo ao Rails no Heroku'
 end
