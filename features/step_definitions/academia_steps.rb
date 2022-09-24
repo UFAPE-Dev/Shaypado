1a5caa20c07cc130f316444205abb82910e11112
Gabriel Viana <gabrielalmeidavn@gmail.com>
2022-09-24 09:56:57 -0300

    Criação dos arquivos de step_definitions com layout base

---
+++ features/step_definitions/academia_steps.rb
@@ -0,0 +1,67 @@
+Given('eu estou na pagina de cadastro de academia') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu preencho o campo nome {string}, email {string}, endereco {string}, cnpj {string} e contato {string}') do |string, string2, string3, string4, string5|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Criar Academia') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que a academia foi criada') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('existe a academia que possui nome {string}, email {string}, endereco {string}, cnpj {string} e contato {string}') do |string, string2, string3, string4, string5|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu estou na pagina das minhas academias') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu visualizo a academia com cnpj {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('eu visualizo a academia com cnpj {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Ver Academia') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu sou redirecionado para a pagina do perfil da academia com cnpj {string} onde eu posso ver seus dados') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('estou na pagina de edicao dessa academia com cnpj {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu edito os campos com os novos valores nome {string}, email {string}, endereco {string}, cnpj {string} e contato {string}') do |string, string2, string3, string4, string5|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Editar Academia') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que a academia foi editada') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('estou na pagina de visualizacao dessa academia com cnpj {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Remover Academia') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que a academia foi removida') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/instrutor_steps.rb
@@ -0,0 +1,67 @@
+Given('eu estou na pagina de cadastro de instrutor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu preencho o campo nome {string}, email {string}, senha {string}, endereco {string}, cep {string}, cpf {string}, data de nascimento {string}, contato {string} e horario trabalho {string}') do |string, string2, string3, string4, string5, string6, string7, string8, string9|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Criar Instrutor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que a instrutor foi criado') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('existe o instrutor que possui nome {string}, email {string}, senha {string}, endereco {string}, cep {string}, cpf {string}, data de nascimento {string}, contato {string} e horario trabalho {string}') do |string, string2, string3, string4, string5, string6, string7, string8, string9|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu estou na pagina dos instrutores') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu visualizo o instrutor com cpf {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('eu visualizo o instrutor com cpf {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Ver Instrutor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu sou redirecionado para a pagina do perfil do instrutor com cpf {string} onde eu posso ver seus dados') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('estou na pagina de edicao desse instrutor com cpf {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu edito os campos com os novos valores nome {string}, email {string}, senha {string}, endereco {string}, cep {string}, cpf {string}, data de nascimento {string}, contato {string} e horario trabalho {string}') do |string, string2, string3, string4, string5, string6, string7, string8, string9|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Editar Instrutor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que o instrutor foi editado') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('estou na pagina de visualizacao desse instrutor com cpf {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('eu clico no botao Remover Instrutor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('eu vejo uma mensagem de sucesso dizendo que o instrutor foi removido') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
