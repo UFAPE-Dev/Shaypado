25c246d62a73990e040c93b99248ca3175a48596
beatrizAVanderlei <anabiaavanderlei@gmail.com>
2022-09-25 15:57:39 -0300

    Alterações no Schema

---
+++ test/models/academia_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class AcademiumTest < ActiveSupport::TestCase
+   test "the truth" do
+     assert true
+  end
+  # teste salvar academia sem dados
+  # teste salvar academia com dados corretos
+  # teste salvar academia sem proprietario
+end
