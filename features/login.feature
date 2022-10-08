35faa76ada878fdcd773ae6c7e73d7069dfe0fea
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2022-10-07 23:12:59 -0300

    Ajustados testes do model de proprietário

--- features/login.feature
+++ features/login.feature
@@ -4,13 +4,13 @@ Feature: login
   So that eu consiga acessar o sistema e realizar operacoes
 
   Scenario: registro com sucesso
-    Given eu estou na pagina de cadastro
+    Given eu estou na pagina de cadastro de usuario
     When preencho meu email 'email@email.com' e senha '123456senha'
     And eu clico no botao Sign Up
     Then eu vejo a mensagem 'Welcome! You have signed up successfully.'
 
   Scenario: login com sucesso
-    Given eu estou na pagina de cadastro
+    Given eu estou na pagina de cadastro de usuario
     When preencho meu email 'email@email.com' e senha '123456senha'
     And eu clico no botao Sign Up
     And eu faco logout
@@ -20,7 +20,7 @@ Feature: login
     Then eu vejo a mensagem 'Signed in successfully.'
 
   Scenario: login com email invalido
-    Given eu estou na pagina de cadastro
+    Given eu estou na pagina de cadastro de usuario
     When preencho meu email 'email@email.com' e senha '123456senha'
     And eu clico no botao Sign Up
     And eu faco logout
@@ -30,7 +30,7 @@ Feature: login
     Then eu vejo a mensagem 'Invalid Email or password.'
 
   Scenario: login com senha invalida
-    Given eu estou na pagina de cadastro
+    Given eu estou na pagina de cadastro de usuario
     When preencho meu email 'email@email.com' e senha '123456senha'
     And eu clico no botao Sign Up
     And eu faco logout
@@ -40,7 +40,7 @@ Feature: login
     Then eu vejo a mensagem 'Invalid Email or password.'
 
   Scenario: login sem senha e email
-    Given eu estou na pagina de cadastro
+    Given eu estou na pagina de cadastro de usuario
     When preencho meu email 'email@email.com' e senha '123456senha'
     And eu clico no botao Sign Up
     And eu faco logout
--- test/models/proprietario_test.rb
+++ test/models/proprietario_test.rb
@@ -9,28 +9,22 @@ class ProprietarioTest < ActiveSupport::TestCase
   #teste de salvar proprietario corretamente
 
   test "salvar proprietario corretamente" do
-    @proprietario = Proprietario.new
-    @user = User.new(nome: "Teste Pedro", password: "123456", password_confirmation: "123456", email: "testeemail@email.com", cpf: "12345678911", endereco: "endereco teste", cep: "55385000", data_nascimento: Date.yesterday, contato: "84756984522", horario_trabalho: "08:15")
-    @user.userable = @proprietario
+    @user = Proprietario.new(nome: "Teste Pedro", password: "123456", password_confirmation: "123456", email: "testeemail@email.com", cpf: "12345678911", endereco: "endereco teste", cep: "55385000", data_nascimento: Date.yesterday, contato: "84756984522")
     assert @user.save, "Todos os dados estão corretos"
   end
 
   #teste de salvar proprietario sem dados
 
   test "salvar proprietario sem dados" do
-    @proprietario = Proprietario.new
-    @user = User.new(nome: "", password: "", password_confirmation: "", email: "", cpf: "", endereco: "", cep: "", data_nascimento: "", contato: "", horario_trabalho: "")
-    @user.userable = @proprietario
+    @user = Proprietario.new(nome: "", password: "", password_confirmation: "", email: "", cpf: "", endereco: "", cep: "", data_nascimento: "", contato: "")
     assert_not @user.save, "Não é possível salvar proprietários sem dados"
   end
 
   #teste de salvar proprietario com cpf incorreto
 
   test "salvar proprietario com cpf incorreto" do
-    @proprietario = Proprietario.new
-    @user = User.new(nome: "Teste Pedro", password: "123456", password_confirmation: "123456", email: "testeemail@email.com", cpf: "a", endereco: "endereco teste", cep: "55385000", data_nascimento: Date.yesterday, contato: "84756984522", horario_trabalho: "08:15")
-    @user.userable = @proprietario
-    assert_not @user.save, "O CPF está incorreto"
+    @user = Proprietario.new(nome: "Teste Pedro", password: "12345678", password_confirmation: "12345678", email: "testeemail@email.com", cpf: "a", endereco: "endereco teste", cep: "55385000", data_nascimento: Date.yesterday, contato: "84756984522")
+    assert_not @user.save, "Não é possível salvar proprietários com cpf incorreto"
   end
 
 end
