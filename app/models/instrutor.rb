25952dd34286eb01c7350edb02b141a899634c2a
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-09 20:31:14 -0300

    Retirar dos validates de cep e endereco do Model de Instrutor

--- app/models/instrutor.rb
+++ app/models/instrutor.rb
@@ -5,8 +5,6 @@ class Instrutor < ApplicationRecord
   validates :nome, presence: true, length: { maximum: 100, minimum: 10 }, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/, message: "Somente letras"}
   validates :cpf, presence: true, length: { is: 11 }, uniqueness: true, format: { with: /\A[0-9]+\z/, message: "Somente números" }
   validates :data_nascimento, presence: true, comparison: {less_than: Time.zone.today, greater_than: Date.new(1850, 1, 1)}
-  validates :cep, presence: true, length: { maximum: 8 }, format: { with: /\A\d{8}\z/ }
-  validates :endereco, presence: true, length: { maximum: 100, minimum: 5 }
   validates :contato, presence: true, length: { is: 11 }, format: { with: /\A[0-9]+\z/, message: "Somente números" }
   validates :horario_trabalho, presence: true
   validates :academium_id, presence: true
