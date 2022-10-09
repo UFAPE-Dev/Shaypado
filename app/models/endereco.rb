6e89d85296bea0ab4281e9ae15d3380bbaacd1ed
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-09 20:50:56 -0300

    Ajustes nos Validates de Endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -4,11 +4,11 @@ class Endereco < ApplicationRecord
   belongs_to :instrutor
 
   # Validacoes
-  validates :logradouro, precense: true, length: { in: 4..100 }
-  validates :numero, precense: true, length: { in: 1..10 }
+  validates :logradouro, presence: true, length: { in: 4..100 }
+  validates :numero, presence: true, length: { in: 1..10 }
   validates :complemento, length: { in: 2..100 }
-  validates :cep, precense: true, with => /\A\d{5}-\d{3}\z/, message: "CEP inválido"
-  validates :bairro, precense: true, length: { in: 2..100 }
-  validates :cidade, precense: true, length: { in: 2..100 }
+  validates :cep, presence: true, with => /\A\d{5}-\d{3}\z/, message: "CEP inválido"
+  validates :bairro, presence: true, length: { in: 2..100 }
+  validates :cidade, presence: true, length: { in: 2..100 }
 
 end
