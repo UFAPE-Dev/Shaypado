9ddf4e33a8466239bddc85f1b5f692c8e5fc419e
beatrizAVanderlei <anabiaavanderlei@gmail.com>
2022-10-09 22:29:22 -0300

    Testes de Model de exercicios

--- test/models/exercicio_test.rb
+++ test/models/exercicio_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ExercicioTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "salvar exercicio sem dados" do
+    exercicio = Exercicio.new(titulo: "", dificuldade: "", calorias: "", descricao: "", quantidade: "", tipo: "", academium_id: "")
+    assert_not exercicio.save, "Não é possível salvar o exercicio sem dados"
+  end
+
+  test "salvar exercicio sem academia" do
+    exercicio = Exercicio.new(titulo: "TITULO", dificuldade: "fácil", calorias: "1500", descricao: "AAAAAAAAAAa", quantidade: "1", tipo: "braço", academium_id: "")
+    assert_not exercicio.save, "Não é possível salvar o exercicio sem academia"
+  end
+
+  test "salvar exercicio sem calorias e descrição" do
+    exercicio = Exercicio.new(titulo: "TITULO", dificuldade: "fácil", calorias: "", descricao: "", quantidade: "1", tipo: "braço", academium_id: "1")
+    assert_not exercicio.save, "Não é possível salvar o exercicio sem dados"
+  end
 end
