e6db61d64431fb1822de0e140f42e9d0180adac6
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-09 21:32:30 -0300

    Remocao de Atributos da Tabela Endereco

--- db/migrate/20221009203717_create_enderecos.rb
+++ db/migrate/20221009203717_create_enderecos.rb
@@ -2,11 +2,7 @@ class CreateEnderecos < ActiveRecord::Migration[7.0]
   def change
     create_table :enderecos do |t|
       t.string :logradouro
-      t.integer :numero
-      t.string :complemento
       t.string :cep
-      t.string :bairro
-      t.string :cidade
 
       t.references :instrutor, foreign_key: true
 
