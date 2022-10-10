0d98ff792da48fbb0f7ae9d5399dfb2036d0b4fa
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-09 23:34:16 -0300

    Resolucao de problema do Codacy do model Aluno

--- test/models/aluno_test.rb
+++ test/models/aluno_test.rb
@@ -11,12 +11,12 @@ class AlunoTest < ActiveSupport::TestCase
   end
 
   test "criar aluno sem academia" do
-    aluno = Aluno.new(nome: 'Josefa', endereco: 'Rua Josefa', cep: '111111111', cpf: '11111111111', data_nascimento: Date.today, contato: '11111111111', senha: '111111', academium_id: '')
+    aluno = Aluno.new(nome: 'Josefa', endereco: 'Rua Josefa', cep: '111111111', cpf: '11111111111', data_nascimento: Time.zone.today, contato: '11111111111', senha: '111111', academium_id: '')
     assert_not aluno.save, "Não é possível salvar um aluno sem academia"
   end
 
   test "criar aluno sem senha" do
-    aluno = Aluno.new(nome: 'Josefa', endereco: 'Rua Josefa', cep: '111111111', cpf: '11111111111', data_nascimento: Date.today, contato: '11111111111', senha: '', academium_id: '1')
+    aluno = Aluno.new(nome: 'Josefa', endereco: 'Rua Josefa', cep: '111111111', cpf: '11111111111', data_nascimento: Time.zone.today, contato: '11111111111', senha: '', academium_id: '1')
     assert_not aluno.save, "Não é possível salvar um aluno sem uma senha de acesso"
   end
 end
