84596cd785b564c6c4a1157f49d1ca4f7edf5223
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-12 01:23:20 -0300

    Alteracao no Teste de Model de Instrutro devido a mudança de nome de atributos presentes em instrutor

--- test/models/instrutor_test.rb
+++ test/models/instrutor_test.rb
@@ -6,19 +6,19 @@ class InstrutorTest < ActiveSupport::TestCase
   end
 
   test "salvar instrutor sem dados" do
-    instrutor = Instrutor.new(nome: '', cpf: '', data_nascimento: '', contato: '', horario_trabalho: '', email: '', password: '', academium_id: '')
+    instrutor = Instrutor.new(nome: '', cpf: '', data_de_nascimento: '', contato_telefone: '', horas_de_trabalho: '', email: '', password: '', academium_id: '')
     assert_not instrutor.save, "Não é possível salvar um instrutor sem dados"
   end
 
   test "salvar um instrutor sem academia" do
-    instrutor = Instrutor.new(nome: 'Marcelino', cpf: '11111111111', data_nascimento: Date.yesterday, contato: '11111111111', horario_trabalho: Time.now, email: 'email@email.com', password: '111111', academium_id: '')
+    instrutor = Instrutor.new(nome: 'Marcelino', cpf: '11111111111', data_de_nascimento: Date.yesterday, contato_telefone: '11111111111', horas_de_trabalho: Time.now, email: 'email@email.com', password: '111111', academium_id: '')
     endereco = Endereco.new(cep: '11111111', logradouro: 'Rua Josefa')
     instrutor.endereco = endereco
     assert_not instrutor.save, "Não é possível salvar um instrutor sem vinculá-lo a uma academia"
   end
 
   test "salvar um instrutor sem email e senha" do
-    instrutor = Instrutor.new(nome: 'Marcelino', cpf: '11111111111', data_nascimento: Date.yesterday, contato: '11111111111', horario_trabalho: Time.now, email: '', password: '', academium_id: 1)
+    instrutor = Instrutor.new(nome: 'Marcelino', cpf: '11111111111', data_de_nascimento: Date.yesterday, contato_telefone: '11111111111', horas_de_trabalho: Time.now, email: '', password: '', academium_id: 1)
     endereco = Endereco.new(cep: '11111111', logradouro: 'Rua Josefa')
     instrutor.endereco = endereco
     assert_not instrutor.save, "Não é possível salvar um instrutor sem e-mail e senha para entrar no sistema"
